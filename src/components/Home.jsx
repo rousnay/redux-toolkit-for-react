e7feb4e2b3ddf47a325d0d1ec1be9e299188324c
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-01-22 23:50:03 +0600

    Inital react-redux useSelector added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import cartSlice from '../data/cartSlice';
 import productList from '../data/productList.json'
 import '../styles/home.scss'
 
 const Home = () => {
-
+  const {cartProductIds} = useSelector((state) => state.cart);
   const {addToCart, removeFromCart} = cartSlice.actions;
   const dispatch = useDispatch();
+
+  console.log(cartProductIds)
   return (
     <div className="container product-catalogue">
       <div className="row">
@@ -20,8 +22,8 @@ const Home = () => {
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
-                  <button className="btn btn-primary" onClick={()=>{dispatch(addToCart(product.id))}}>Add to cart</button>
-                  <button className="btn btn-secondary" onClick={()=>{dispatch(removeFromCart(product.id))}}>Remove from cart</button>
+                  { !cartProductIds.includes(product.id) && (<button className="btn btn-primary" onClick={()=>{dispatch(addToCart(product.id))}}>Add to cart</button>)}
+                  {cartProductIds.includes(product.id) && (<button className="btn btn-secondary" onClick={()=>{dispatch(removeFromCart(product.id))}}>Remove from cart</button>)}
                 </div>
               </div>
             </div>
